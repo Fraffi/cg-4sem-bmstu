1ae51f40034c2185203a564a303752d9c883cb09
Platonovich Egor <eplatonovich@mail.ru>
2021-02-28 20:23:52 +0300

    New drawing functions were added.

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+import {dot, draw_tri, getRndColor} from './funcs.js';
+
+dot(getRndColor(),30, 30);
+dot(getRndColor(), 40, 50);
+
+draw_tri(10, 10, 20, 80, 60, 50);
+draw_tri(20, 20, 30, 80, 60, 50);
