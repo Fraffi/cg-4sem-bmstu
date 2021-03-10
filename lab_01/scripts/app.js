b64616b368006c4029b62659f4d4abaaf11330e3
Platonovich Egor <eplatonovich@mail.ru>
2021-03-10 18:31:03 +0300

    Some new checks were added.

--- lab_01/scripts/app.js
+++ lab_01/scripts/app.js
@@ -2,11 +2,30 @@ import {dot, draw_on_dots, getRndColor} from './funcs.js';
 
 let dots = [];
 let dots_counter = 0;
+
+function check_in_arr(x, y, arr)
+{
+    let in_flag = 0;
+    for (let i = 0; i < arr.length; i++)
+    {
+        if ((arr[i].x === x) && (arr[i].y === y))
+        {
+            in_flag = 1;
+        }
+    }
+    return in_flag;
+}
+
 function push_coords(x, y, i)
 {
-    dots[i] = {};
-    dots[i].x = x;
-    dots[i].y = y;
+    let in_flag = check_in_arr(x, y, dots);
+    if (in_flag === 0)
+    {
+        dots[i] = {};
+        dots[i].x = x;
+        dots[i].y = y;
+    }
+    return in_flag;
 }
 const canvas = document.querySelector('canvas')
 if (canvas.getContext) {
@@ -21,16 +40,19 @@ function getCursorPosition(ctx, canvas, event)
     return[x, y];
 }
 
-function on_click_func(ctx, canvas, e)
+function on_click_func(ctx, canvas, evt)
 {
-    let coords = getCursorPosition(ctx, canvas, e);
-    push_coords(coords[0], coords[1], dots_counter);
-    dot(ctx, getRndColor(), dots[dots_counter].x, dots[dots_counter].y);
-    dots_counter += 1;
-    draw_on_dots(dots_counter, ctx, dots);
+    let coords = getCursorPosition(ctx, canvas, evt);
+    let in_flag = push_coords(coords[0], coords[1], dots_counter);
+    if (in_flag === 0)
+    {
+        dot(ctx, getRndColor(), dots[dots_counter].x, dots[dots_counter].y);
+        dots_counter += 1;
+        draw_on_dots(dots_counter, ctx, dots);
+    }
 }
 
-canvas.addEventListener('mousedown', function(evt) {
+canvas.addEventListener('click', function(evt) {
     on_click_func(ctx, canvas, evt);
 });
 
