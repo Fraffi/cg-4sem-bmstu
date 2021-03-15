81080928f608bdc324c56d19b8bc5039d883a4e4
Platonovich Egor <eplatonovich@mail.ru>
2021-03-16 00:38:44 +0300

    zooming doots algoritm was remastered

--- lab_01/scripts/app.js
+++ lab_01/scripts/app.js
@@ -1,5 +1,6 @@
 import {dot, draw_on_dots, reset_all} from './funcs.js';
 
+//Check, array functions and definitions
 let dots = [];
 
 function check_in_arr(x, y, arr)
@@ -43,6 +44,8 @@ function getCursorPosition(ctx, canvas, event)
     return[x, y];
 }
 
+//BUTTONS
+//coordinates buttons functions
 function new_btn(ctx, x, y)
 {
     let new_btn = document.createElement('button');
@@ -52,36 +55,14 @@ function new_btn(ctx, x, y)
     new_btn.addEventListener('click', function() {
         on_click_coords_b(ctx, new_btn.id);
     });
-    new_btn.addEventListener('blur', function() {
+    new_btn.addEventListener('mouseover', function() {
+        on_click_coords_b_zoom(ctx, new_btn.id);
+    });
+    new_btn.addEventListener('mouseout', function() {
         reset_all(ctx, canvas, dots);
     });
     co_btn.appendChild(new_btn);
 }
-
-function on_click_func(ctx, canvas, evt)
-{
-    let coords = getCursorPosition(ctx, canvas, evt);
-    let in_flag = push_coords(coords[0], coords[1], dots.length);
-    if (in_flag === 0)
-    {
-        dot(ctx, dots[dots.length - 1].x, dots[dots.length - 1].y, 1, 'red', 'black', 2);
-        new_btn(ctx, dots[dots.length - 1].x, dots[dots.length - 1].y);
-        draw_on_dots(ctx, dots, dots.length);
-    }
-}
-
-canvas.addEventListener('click', function(evt) {
-    on_click_func(ctx, canvas, evt);
-});
-
-$(document).click(function(e){
-    let task = $('.task-menu');
-    if ( $(e.target).closest('.task-button').length) {
-        task.toggleClass('task_show');
-        return;
-    }
-    task.removeClass('task_show');
-});
 
 function on_click_coords_b_del(ctx, name) {
     for (let i = 0; i < dots.length; i++)
@@ -111,25 +92,57 @@ function on_click_coords_b_zoom(ctx, name)
 
 function on_click_coords_b(ctx, name) {
     let coords = $('.coordinates-buttons');
-    let btn = document.getElementById(name);
+    //let btn = document.getElementById(name);
     if (coords.hasClass('to-delete')) {
         on_click_coords_b_del(ctx, name);
     }
-    if (document.activeElement === btn)
+    /*if (document.activeElement === btn)
     {
         on_click_coords_b_zoom(ctx, name);
-    }
+    }*/
 }
 
+
+//JQuery buttons functions
+//Task button
+$(document).click(function(e){
+    let task = $('.task-menu');
+    if ( $(e.target).closest('.task-button').length) {
+        task.toggleClass('task_show');
+        return;
+    }
+    task.removeClass('task_show');
+});
+
+//Remove button
 $('.rem-button').click(function() {
     $('.rem-button').toggleClass('rem-button-active');
     $('.coordinates-buttons').toggleClass('to-delete');
 });
 
+//Submit button
 $('.sub-button').click(function() {
     $('.sub-button').addClass('sub-button-active').delay(250).queue(function () {
         let sbt_a = $('.sub-button');
         sbt_a.removeClass('sub-button-active');
         sbt_a.dequeue();
     })
+});
+
+
+//Canvas functions
+function on_click_func(ctx, canvas, evt)
+{
+    let coords = getCursorPosition(ctx, canvas, evt);
+    let in_flag = push_coords(coords[0], coords[1], dots.length);
+    if (in_flag === 0)
+    {
+        dot(ctx, dots[dots.length - 1].x, dots[dots.length - 1].y, 1, 'red', 'black', 2);
+        new_btn(ctx, dots[dots.length - 1].x, dots[dots.length - 1].y);
+        draw_on_dots(ctx, dots, dots.length);
+    }
+}
+
+canvas.addEventListener('click', function(evt) {
+    on_click_func(ctx, canvas, evt);
 });
